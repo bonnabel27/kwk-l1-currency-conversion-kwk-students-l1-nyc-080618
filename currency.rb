695c9c993dafa2a843829dd6843f5bb65cce03c5
bonnabel27 <sika.attikesse@gmail.com>
2018-08-10 14:35:29 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,6 +1,1 @@
-
-  
-  it "should convert dollars to euros and back" do
-    expect(eur_to_usd(usd_to_eur(1.0))).to eql(1.0), "Dollars (usd) did not correctly convert to euros (eur) and back to dollars"
-  end
-end
+def usd_to_
