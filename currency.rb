41db220e40a67027eb530e7e21bc2ad7ad14537e
bonnabel27 <sika.attikesse@gmail.com>
2018-08-10 14:48:13 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -2,8 +2,10 @@ def usd_to_eur(input)
   coins = input.to_i * 0.88
   return coins
 end
-puts
+puts usd_to_eur
+
 def eur_to_usd (input)
   coins = input.to_i / 0.88
   return coins
 end
+puts eur_to_usd
